786c5dc72a03b66446dd74ae81f436a4d41c7f61
Mohamed Sadat <mohamed.sadat@frichti.co>
2020-07-01 16:21:17 +0200

    Add linting scripts

--- src/index.ts
+++ src/index.ts
@@ -20,36 +20,34 @@ interface IState {
 
 const pluginName = 'babel-plugin-disallow-imports-plugin'
 
-function shouldThrowError({
+function shouldThrowError ({
   disallowedImporters,
   allowedImporters,
-  filename,
+  filename
 }: {
   disallowedImporters?: string[],
   allowedImporters?: string[],
   filename: string,
 }): boolean {
-  if (!!disallowedImporters
-    && disallowedImporters.some((importer: string) => filename.indexOf(importer) !== -1)) {
-      return true
+  if (!!disallowedImporters && disallowedImporters.some((importer: string) => filename.indexOf(importer) !== -1)) {
+    return true
   }
 
-  if (!!allowedImporters
-    && allowedImporters.every((importer: string) => filename.indexOf(importer) < 0)) {
+  if (!!allowedImporters && allowedImporters.every((importer: string) => filename.indexOf(importer) < 0)) {
     return true
   }
 
   return false
 }
 
-function warn(message: string) {
+function warn (message: string) {
   console.warn(message)
 }
 
-export default function DisallowImportsPlugin(): PluginObj<any> {
+export default function DisallowImportsPlugin (): PluginObj<any> {
   return {
     visitor: {
-      ImportDeclaration(path: any, state: IState) {
+      ImportDeclaration (path: any, state: IState) {
         if (!state.opts.properties || !Array.isArray(state.opts.properties)) {
           warn(`No properties array has been provided to ${pluginName}.\nMake sure to provide an array of properties`)
           return
@@ -60,31 +58,32 @@ export default function DisallowImportsPlugin(): PluginObj<any> {
             message = 'An uknown disallowed import brokes the transpiling...',
             disallowedImport,
             disallowedImporters,
-            allowedImporters,
+            allowedImporters
           } = property
-          if(!Boolean(disallowedImport)) {
+          if (!disallowedImport) {
             throw new Error(`No disallowedImport has been provided to ${pluginName}.`)
           }
-  
-          if (!Boolean(message)) {
-            warn(`No custom message were provided.\nPlease add a custom error message for more readability.`)
+
+          if (!message) {
+            warn(`No custom message were provided.
+            \nPlease add a custom error message for more readability.`)
           }
-  
+
           if (Boolean(disallowedImporters) && Boolean(allowedImporters)) {
             warn(`You provided both disallowedImporters and allowedImporters.\nThe ${pluginName} will take only disallowedImporters.`)
           }
-  
+
           const source = path.node.source.value
           if (source !== disallowedImport) {
             return
           }
-  
+
           const { filename } = state.file.opts
           if (shouldThrowError({ disallowedImporters, allowedImporters, filename })) {
             throw new Error(message)
           }
         })
-      },
-    },
+      }
+    }
   }
 }
